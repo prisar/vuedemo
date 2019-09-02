5d1cfa304f122a6d7408a6beaba9e972e14ba2f9
Pritimay Sarkar <pritimay.sarkar@rupeek.com>
2019-09-02 12:44:07 +0530

    add saleProducts property

--- src/components/ProductListTwo.vue
+++ src/components/ProductListTwo.vue
@@ -1,8 +1,8 @@
 <template>
-  <div id="product-list-one">
+  <div id="product-list-two">
     <h2>Product List Two</h2>
     <ul>
-      <li v-for="product in products">
+      <li v-for="product in saleProducts">
         <span class="name">{{ product.name }}</span>
         <span class="price">${{ product.price }}</span>
       </li>
@@ -15,6 +15,16 @@ export default {
     computed: {
         products(){
             return this.$store.state.products;
+        },
+        saleProducts() {
+            var saleProducts = this.$store.state.products.map( product => {
+                return {
+                    name: '**' + product.name + '**',
+                    price: product.price / 2
+                }
+            })
+
+            return saleProducts;
         }
     }
 };
