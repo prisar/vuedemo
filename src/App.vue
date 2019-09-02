54c26a52eb3a846e60d87ed23bd95640c0db6442
Pritimay Sarkar <pritimay.sarkar@rupeek.com>
2019-09-02 12:11:10 +0530

    add two components

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,37 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App redeploy"/>
+    <product-list-one v-bind:products="products"></product-list-one>
+    <product-list-two v-bind:products="products"></product-list-two>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import ProductListOne from './components/ProductListOne';
+import ProductListTwo from './components/ProductListTwo';
 
 export default {
-  name: 'app',
   components: {
-    HelloWorld
+    'product-list-one': ProductListOne,
+    'product-list-two': ProductListTwo
+  },
+  name: 'app',
+  data() {
+    return {
+      products: [
+        { name: 'Banana Skin', price: 20 },
+        { name: 'Shiny Star', price: 40 },
+        { name: 'Green Shells', price: 60 },
+        { name: 'Red Shells', price: 80 }
+      ]
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body{
+    font-family: Ubuntu;
+    color: #555;
 }
 </style>
